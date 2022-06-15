4e6e244a718e4f248556d2644867cb865fa06f99
shuraikhhh <49565432+shuraikhhh@users.noreply.github.com>
2022-06-16 04:41:35 +0800

    Update apapa.py

--- apapa.py
+++ apapa.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-from sklearn.ensemble import RandomForestClassifier
+#from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
 # Simple Iris Flower Prediction App
