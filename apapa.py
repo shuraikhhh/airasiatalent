9d54ffcbb00e3e5aa89c6f85371c797b9336bf21
shuraikhhh <49565432+shuraikhhh@users.noreply.github.com>
2022-06-16 04:33:22 +0800

    Update apapa.py

--- apapa.py
+++ apapa.py
@@ -41,10 +41,12 @@ st.subheader('Class labels and their corresponding index number')
 st.write(iris.target_names)
 
 st.subheader('Prediction')
-st.write(iris.target_names[prediction])
-#st.write(prediction)
+#st.write(iris.target_names[prediction])
+st.write(prediction)
 
 st.subheader('Prediction Probability')
 st.write(prediction_proba)
+
+
 
 
