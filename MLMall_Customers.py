1545080e656cefde1803cd356b5d978cf57f7ad6
shuraikhhh <49565432+shuraikhhh@users.noreply.github.com>
2022-06-16 19:03:03 +0800

    Update MLMall_Customers.py

--- MLMall_Customers.py
+++ MLMall_Customers.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-%matplotlib inline
+
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
