8d1c1faa609b668e52f9454c85b0fce7104714a7
shuraikhhh <49565432+shuraikhhh@users.noreply.github.com>
2022-06-16 18:57:51 +0800

    Create MLMall_Customers.py

---
+++ MLMall_Customers.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+
+df = pd.read_csv('https://raw.githubusercontent.com/shuraikhhh/airasiatalent/main/Mall_Customers.csv')
+
+df.rename(columns={"Genre":"Gender"}, inplace=True)
+df.rename(columns={"Annual Income (k$)":"Annual Income"}, inplace=True)
+df.rename(columns={"Spending Score (1-100)":"Spending Score"}, inplace=True)
+
